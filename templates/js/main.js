9fe7c010ac5580bb1c8c9bd61ec4d303073d3a7f
Eseoghene Eguakhide <eruotorese@yahoo.com>
2022-11-13 14:06:24 +0100

    bbc clone

---
+++ templates/js/main.js
@@ -0,0 +1,7 @@
+document.querySelector(`body`).onclick = black
+
+function black(){
+  document.querySelector(`body`).style.backgroundColor = `pink`
+  document.querySelector(`h2`).style.fontStyle=`italic`
+  document.querySelector(`nav`).style.backgroundColor = `grey`
+}
